6616b3e784a402b06b771d8c5fa3e8c2eb8973df
Gabor <orbangabeszb@gmail.com>
2019-12-24 15:05:40 +0100

    uri components printed

--- src/com/orgabor/NWDemoMain.java
+++ src/com/orgabor/NWDemoMain.java
@@ -8,6 +8,17 @@ public class NWDemoMain {
 	public static void main(String[] args) {
 		try {
 			URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=androidamsung");
+			
+			System.out.println("Scheme = " + uri.getScheme());
+			System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+			System.out.println("Authority = " + uri.getAuthority());
+			System.out.println("User info = " + uri.getUserInfo());
+			System.out.println("Host = " + uri.getHost());
+			System.out.println("Port = " + uri.getPort());
+			System.out.println("Path = " + uri.getPath());
+			System.out.println("Query = " + uri.getQuery());
+			System.out.println("Fragment = " + uri.getFragment());
+			
 		} catch(URISyntaxException e) {
 			System.out.println("Bad URI Syntax: " + e.getMessage());
 		}
