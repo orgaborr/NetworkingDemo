cda39f61b51ff8d90fc5ece66a22f772649c25e7
Gabor <orbangabeszb@gmail.com>
2019-12-24 15:01:06 +0100

    URI initiated

--- src/com/orgabor/NWDemoMain.java
+++ src/com/orgabor/NWDemoMain.java
@@ -1,10 +1,16 @@
 package com.orgabor;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class NWDemoMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=androidamsung");
+		} catch(URISyntaxException e) {
+			System.out.println("Bad URI Syntax: " + e.getMessage());
+		}
 	}
 
 }
